1b090238b8c650e5965a39b20b4a2090509178cd
AbidemiT <tiamiyusikiruabidemi@gmail.com>
2023-07-27 05:57:18 +0100

    Reuseable Input and Select Field

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,20 @@
+<template>
+    <label>{{ label }}</label>
+    <!-- Manually bind input attributes with ($attrs) -->
+    <input v-bind="$attrs" :placeholder="label" class="field" :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)">
+</template>
+
+<script setup>
+defineEmits(['update:modelValue']);
+defineProps({
+    label: {
+        type: String,
+        default: ''
+    },
+    modelValue: {
+        type: [String, Number],
+        default: ''
+    }
+});
+</script>
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,24 @@
+<template>
+    <label>{{ label }}</label>
+    <select @change="$emit('update:modelValue', $event.target.value)">
+        <option v-for="(option, i) in options" :key="option + '-' + i" :value="option" :selected="option === modelValue">{{ option }}</option>
+    </select>
+</template>
+
+<script setup>
+defineEmits(['update:modelValue']);
+defineProps({
+    label: {
+        type: String,
+        default: ''
+    },
+    modelValue: {
+        type: [String, Number],
+        default: ''
+    },
+    options: {
+        type: Array,
+        default: ["Select Option"]
+    }
+});
+</script>
